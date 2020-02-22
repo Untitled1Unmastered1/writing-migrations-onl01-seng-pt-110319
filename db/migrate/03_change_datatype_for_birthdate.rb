d296786d25ed571deb9b3aa4490f15a7fc0ec10f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-22 04:23:20 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
-# class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-#   def change 
-#     change_column :birthdate, :datetime, :string 
-#   end
-# end 
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  def change 
+    change_column :birthdate, :datetime, :string 
+  end
+end 
     
